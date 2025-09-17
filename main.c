80e7988b2f511a1f0f1f0de5b1d48102e10eaa04
tarek-talsi <tarek.talsi.430@gmail.com>
2025-09-17 09:22:20 +0200

    1st commit test

---
+++ main.c
@@ -0,0 +1,25 @@
+
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+
+    int variable_DATA = 13;
+    int variable_BSS=0;
+    auto char* variable_STR = "je suis un message bienveillant";
+    int* variable_heap=malloc(sizeof (int variable_DATA));
+    void function_text();
+
+    printf(%)
+
+
+
+
+
+
+
+    //printf("%p,\n",variable_DATA);
+    }
+
+
+    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+
