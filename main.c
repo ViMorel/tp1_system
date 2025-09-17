adeb823392c952c8541b6e9ca3c493d54b3f7551
tarek-talsi <tarek.talsi.430@gmail.com>
2025-09-17 09:48:29 +0200

    update

--- main.c
+++ main.c
@@ -1,23 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
 
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
 
-    int variable_DATA = 13;
-    int variable_BSS=0;
-    auto char* variable_STR = "je suis un message bienveillant";
-    int* variable_heap=malloc(sizeof (int variable_DATA));
+    static int variable_DATA = 13;
+    static int variable_BSS=0;
+    char* variable_STR = "je suis un message bienveillant";
+
+    int* variable_heap = malloc(sizeof(int));
+    *variable_heap = 53;
     void function_text();
-
-    printf(%)
-
-
-
-
-
-
-
-    //printf("%p,\n",variable_DATA);
+    void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
+    printf("%p \n", (void*)&variable_DATA);
+    printf("%p \n", (void*)&variable_BSS);
+    printf("%p \n", (void*)variable_STR);
+    printf("%p \n", (void*)variable_heap);
+    printf("%p \n", (void*)function_text);
+    printf("%p \n", (void*)mmap);
     }
 
 
